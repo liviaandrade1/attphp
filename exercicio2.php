46d478828c073556dd9e5370a4eac176e5b02d88
liviaandrade1 <livia.oliveira166@etec.sp.gov.br>
2025-04-01 22:14:24 -0300

    Update exercicio2.php

--- exercicio2.php
+++ exercicio2.php
@@ -1,9 +1,12 @@
 <?php 
 	$num = 100;
-	if ($num1 = $num %10 == 0){
-		print($num);
-	}
-
-
-
- ?>
+	if ($num%10==0){
+		echo "É divisível por 10";
+	}elseif ($num%5==0){
+		echo "É divisível por 5";
+	}elseif ($num%2==0){
+		echo "É divisível por 2";
+	}else
+		echo "Não é divisível por nenhuma das opções.";
+ 
+?>
